337a592a987bba53cf1097101e41de74a6f6397d
14d_kincsa <14d_kincsa@T202-PC02.NYIRSZIKSZI.local>
2017-12-18 13:19:51 +0100

    my messagedsadsaasddsa

---
+++ JavaApplication13/src/javaapplication13/JavaApplication13.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication13;
+
+/**
+ *
+ * @author 14d_kincsa
+ */
+public class JavaApplication13 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     
+        //asdasdasdsadsadasdsdasdsasdaadsasddasads
+        
+    }
+    
+}
